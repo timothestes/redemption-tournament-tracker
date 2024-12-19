0da32365b396d5da41150f754a321870c0366f7f
Tim Estes <33536315+timothestes@users.noreply.github.com>
2024-12-18 21:39:23 -0800

    style: Adjust side navigation width from 64 to 40 pixels

--- components/side-nav.tsx
+++ components/side-nav.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SideNav: React.FC = () => {
   return (
-    <nav className="fixed top-16 left-0 h-full w-64 bg-gray-800 text-white p-4 shadow-lg">
+    <nav className="fixed top-16 left-0 h-full w-40 bg-gray-800 text-white p-4 shadow-lg">
       <ul>
         <li className="mb-2">
           <a href="/protected/tournaments" className="hover:underline">
