0c785a921514eef5e949868a7ca30bc0c3033ccf
Tim Estes <33536315+timothestes@users.noreply.github.com>
2024-12-18 21:41:04 -0800

    add side nav to tournaments

--- app/protected/tournaments/page.tsx
+++ app/protected/tournaments/page.tsx
@@ -1,6 +1,9 @@
+import SideNav from "../../../components/side-nav";
+
 export default function TournamentsPage() {
   return (
     <div className="p-4">
+      <SideNav />
       <h1 className="text-2xl font-bold">Your tournaments</h1>
     </div>
   );
