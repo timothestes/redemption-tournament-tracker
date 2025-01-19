eef8ce7bf62441fbc18014b0a6f506a4a31fc8b2
Tim Estes <33536315+timothestes@users.noreply.github.com>
2025-01-19 14:50:06 -0800

    Update background.tsx

--- components/ui/background.tsx
+++ components/ui/background.tsx
@@ -8,11 +8,10 @@ const Background: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
         backgroundImage: "url('/login-splash.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
-        transform: "scale(0.5)",
         transformOrigin: "center",
       }}
     >
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="absolute inset-0 bg-black bg-opacity-70"></div>
       <div className="relative z-10">{children}</div>
     </div>
   );
