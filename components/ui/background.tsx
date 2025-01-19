e607b9ce06ce1e6489a52a1adf0283d52cf85457
Tim Estes <33536315+timothestes@users.noreply.github.com>
2025-01-19 14:55:34 -0800

    Update background.tsx

--- components/ui/background.tsx
+++ components/ui/background.tsx
@@ -11,7 +11,7 @@ const Background: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
         transformOrigin: "center",
       }}
     >
-      <div className="absolute inset-0 bg-black bg-opacity-70"></div>
+      <div className="absolute inset-0 bg-black bg-opacity-60"></div>
       <div className="relative z-10">{children}</div>
     </div>
   );
