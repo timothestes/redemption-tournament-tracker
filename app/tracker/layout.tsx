147c4ff584e7fe3b8060689ed7201e6b871ee749
Mohid Faizi <mohidfaizipro@gmail.com>
2025-02-08 10:38:10 +0500

    Navigation menu fix

--- app/tracker/layout.tsx
+++ app/tracker/layout.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-import SideNav from "../../components/side-nav";
-
 export default function TournamentsLayout({ children }) {
-  return (
-    <div className="flex h-screen">
-      <SideNav />
-      <div className="flex-grow">{children}</div>
-    </div>
-  );
+  return <div className="w-full flex">{children}</div>;
 }
--- app/tracker/tournaments/page.tsx
+++ app/tracker/tournaments/page.tsx
@@ -16,7 +16,8 @@ export default function TournamentsPage() {
   const [loading, setLoading] = useState(true);
   const [showDeleteToast, setShowDeleteToast] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  const [isAddTournamentModalOpen, setisAddTournamentModalOpen] = useState(false);
+  const [isAddTournamentModalOpen, setisAddTournamentModalOpen] =
+    useState(false);
   const [currentTournament, setCurrentTournament] = useState(null);
   const [newTournamentName, setNewTournamentName] = useState("");
   const router = useRouter();
@@ -90,36 +91,34 @@ export default function TournamentsPage() {
   };
 
   return (
-    <div className="flex h-screen pl-64">
-      <div className="max-w-4xl mx-auto">
+    <div className="flex h-screen px-5 w-full">
+      <div className="max-w-5xl mx-auto w-full">
         <Breadcrumb
-          items={[
-            { label: "Tournaments", href: "/tracker/tournaments" }
-          ]}
+          items={[{ label: "Tournaments", href: "/tracker/tournaments" }]}
         />
-      <div className="flex items-center justify-between mb-6">
-        <h1 className="text-2xl font-bold mr-8 mt-2">Your Tournaments</h1>
-        <Button
-          onClick={() => setisAddTournamentModalOpen(true)}
-          className="flex items-center gap-2 mt-2"
-          outline
-          gradientDuoTone="greenToBlue"
-        >
-          <HiPlus className="w-5 h-5" />
-          Host a Tournament
-        </Button>
-        <TournamentFormModal
-          isOpen={isAddTournamentModalOpen}
-          onClose={() => setisAddTournamentModalOpen(false)}
-          onSubmit={handleAddTournament}
-        />
-      </div>
+        <div className="flex items-center justify-between flex-wrap mb-6">
+          <h1 className="text-2xl font-bold mr-8 mt-2">Your Tournaments</h1>
+          <Button
+            onClick={() => setisAddTournamentModalOpen(true)}
+            className="flex items-center gap-3 mt-2"
+            outline
+            gradientDuoTone="greenToBlue"
+          >
+            <div className="flex items-center gap-1">
+              <HiPlus className="w-4 h-4" />
+              <span>Host a Tournament</span>
+            </div>
+          </Button>
+          <TournamentFormModal
+            isOpen={isAddTournamentModalOpen}
+            onClose={() => setisAddTournamentModalOpen(false)}
+            onSubmit={handleAddTournament}
+          />
+        </div>
         {loading ? (
           <p>Loading tournaments...</p>
         ) : tournaments.length === 0 ? (
-          <p>
-            No tournaments found.
-          </p>
+          <p>No tournaments found.</p>
         ) : (
           <div className="overflow-x-auto">
             <Table hoverable>
@@ -130,7 +129,7 @@ export default function TournamentsPage() {
                   <span className="sr-only">Actions</span>
                 </Table.HeadCell>
               </Table.Head>
-              <Table.Body className="divide-y">
+              <Table.Body className="divide-y w-full overflow-auto">
                 {tournaments.map((tournament) => (
                   <Table.Row
                     key={tournament.id}
@@ -142,7 +141,7 @@ export default function TournamentsPage() {
                     <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                       {tournament.name}
                     </Table.Cell>
-                    <Table.Cell>
+                    <Table.Cell className="whitespace-nowrap min-w-[160px]">
                       {new Intl.DateTimeFormat("en-US", {
                         year: "numeric",
                         month: "long",
