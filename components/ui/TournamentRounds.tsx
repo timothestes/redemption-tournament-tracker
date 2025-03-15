9316b93b63edfad95ebd904b734424de3ca642be
Tim Estes <33536315+timothestes@users.noreply.github.com>
2025-03-14 21:43:47 -0700

    prettify participant table

--- components/ui/TournamentRounds.tsx
+++ components/ui/TournamentRounds.tsx
@@ -476,7 +476,7 @@ export default function TournamentRounds({
                       {matches.length > 0 &&
                         matches.map((match, index) => (
                           <Fragment key={match.id}>
-                            <tr className={`border-b border-gray-400/70 ${matchErrorIndex.includes(index) && "bg-red-600/20"}`}>
+                            <tr className={`border-b border-gray-400/70 ${matchErrorIndex.includes(index) ? "bg-red-600/20" : "bg-slate-800"}`}>
                               <td className={`px-4 py-2 text-center border-r ${matchErrorIndex.includes(index) ? "border-red-400" : "border-zinc-400"}`}>
                                 {index + 1}
                               </td>
@@ -503,7 +503,7 @@ export default function TournamentRounds({
                                 />
                               </td>
                             </tr>
-                            <tr className={`border-b border-gray-300 ${matchErrorIndex.includes(index) && "bg-red-600/20"}`}>
+                            <tr className={`border-b border-gray-300 ${matchErrorIndex.includes(index) ? "bg-red-600/20" : "bg-slate-700"}`}>
                               <td className={`px-4 py-2 text-center border-r ${matchErrorIndex.includes(index) ? "border-red-400" : "border-zinc-400"}`}>
                                 {index + 1}
                               </td>
@@ -559,7 +559,6 @@ export default function TournamentRounds({
                             Differential
                           </th>
                           <th scope="col" className="px-4 py-2 text-right">
-                            <span className="sr-only">Actions</span>
                           </th>
                         </tr>
                       </thead>
@@ -567,7 +566,7 @@ export default function TournamentRounds({
                         {byes.length > 0 &&
                           byes.map((bye, index) => (
                             <Fragment key={bye.id}>
-                              <tr className="border-b border-gray-400/70">
+                              <tr className="border-b border-gray-400/70 bg-slate-800">
                                 <td className="px-4 py-2 text-center border-r border-zinc-400">
                                   {index + 1}
                                 </td>
@@ -583,12 +582,7 @@ export default function TournamentRounds({
                                 <td className="px-4 py-2 text-center border-r border-zinc-400">
                                   {bye.differential}
                                 </td>
-                                {/* <td className="px-2">
-                                <MatchEditModal
-                                  match={match}
-                                  fetchCurrentRoundData={fetchCurrentRoundData}
-                                />
-                              </td> */}
+                                <td></td>
                               </tr>
 
                             </Fragment>
