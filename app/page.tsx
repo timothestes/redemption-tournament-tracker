16e81e56d447b0d4978a89317d1d99fcd2c580e8
Tim Estes <33536315+timothestes@users.noreply.github.com>
2026-02-26 09:35:27 -0800

    fix forgot password flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import { redirect } from "next/navigation";
 
-export default async function Index() {
+export default async function Index(props: {
+  searchParams: Promise<{ code?: string }>;
+}) {
+  const searchParams = await props.searchParams;
+  if (searchParams.code) {
+    redirect(
+      `/auth/callback?code=${encodeURIComponent(searchParams.code)}&redirect_to=/tracker/reset-password`,
+    );
+  }
   redirect("/decklist/community");
 }
