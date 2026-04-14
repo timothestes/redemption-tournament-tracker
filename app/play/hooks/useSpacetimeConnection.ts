02f99b8f7b488eb8f3366e4928ddf4aaa4d451a3
Tim Estes <33536315+timothestes@users.noreply.github.com>
2026-04-13 21:47:58 -0700

    Update useSpacetimeConnection.ts

--- app/play/hooks/useSpacetimeConnection.ts
+++ app/play/hooks/useSpacetimeConnection.ts
@@ -1,5 +1,20 @@
 'use client';
 
+// Polyfill Promise.withResolvers for browsers that lack ES2024 support
+// (Safari < 17.4, Chrome < 119, Firefox < 121).
+// The SpacetimeDB SDK uses this internally in callReducer / callProcedure.
+if (typeof Promise.withResolvers === 'undefined') {
+  (Promise as any).withResolvers = function <T>() {
+    let resolve!: (value: T | PromiseLike<T>) => void;
+    let reject!: (reason?: any) => void;
+    const promise = new Promise<T>((res, rej) => {
+      resolve = res;
+      reject = rej;
+    });
+    return { promise, resolve, reject };
+  };
+}
+
 import { useMemo, useState, useCallback } from 'react';
 import { DbConnection } from '@/lib/spacetimedb/module_bindings';
 
